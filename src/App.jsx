052ad7253dee5a608308770ff9c74ee09c30342f
Myro <myroslav.galavai@gmail.com>
2026-03-09 13:28:01 +0100

    Remove reactive node demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, NavLink, Navigate, Route, Routes } from 'react-router-dom'
 import { Agentation } from 'agentation'
 import DigitalKeyPage from './components/DigitalKeyPage'
-import ReactiveNodePage from './components/ReactiveNodePage'
 import TEDMXFieldControllerPage from './components/TEDMXFieldControllerPage'
 
 function TopNav() {
   return (
     <nav className="app-nav" aria-label="Views">
-      <NavLink to="/reactive-node">Reactive Node</NavLink>
       <NavLink to="/digital-key">Digital Key</NavLink>
       <NavLink to="/te-dmx">TE-DMX Controller</NavLink>
     </nav>
@@ -19,8 +17,7 @@ function App() {
     <BrowserRouter>
       <TopNav />
       <Routes>
-        <Route path="/" element={<Navigate to="/reactive-node" replace />} />
-        <Route path="/reactive-node" element={<ReactiveNodePage />} />
+        <Route path="/" element={<Navigate to="/te-dmx" replace />} />
         <Route path="/digital-key" element={<DigitalKeyPage />} />
         <Route path="/te-dmx" element={<TEDMXFieldControllerPage />} />
       </Routes>
